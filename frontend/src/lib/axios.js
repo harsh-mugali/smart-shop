d1839942959a2f35c876c91fb040d1269aad2e62
harsh <mugaliharsh@gmail.com>
2025-09-22 21:48:49 +0530

    added link

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "" : "/api",
+	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "https://smart-shop-3aog.onrender.com/api",
 	withCredentials: true, // send cookies to the server
 });
 
