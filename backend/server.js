23170c1607b931a06ab18d9ae43fe944b403cacd
harsh <mugaliharsh@gmail.com>
2025-09-22 21:58:45 +0530

    added cors setting

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
